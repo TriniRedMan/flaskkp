d26ca20efb0ce2b97f12e1b4b6197d8a027bb960
TriniRedMan <44338527+TriniRedMan@users.noreply.github.com>
2023-12-05 09:51:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from ftplib import FTP
+import os
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+# Configure file uploads
+uploads = UploadSet("uploads", IMAGES)
+app.config["UPLOADED_UPLOADS_DEST"] = "uploads"
+configure_uploads(app, uploads)
 
-if __name__ == '__main__':
+
+ftp_directory = "/public_html/assets"
+ftp_host = "trinicoding.com"
+ftp_user = "admin2@trinicoding.com"
+ftp_password = "Rabbit12"
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST" and "uploads" in request.files:
+        file = request.files["uploads"]
+        if file:
+            # Save the file locally
+            filename = os.path.join(app.config["UPLOADED_UPLOADS_DEST"], file.filename)
+            file.save(filename)
+
+            # Upload the file to FTP
+            upload_to_ftp(filename, ftp_host, ftp_directory, ftp_user, ftp_password)
+
+            # Remove the local file after uploading
+            os.remove(filename)
+
+    return render_template("index.html")
+
+def upload_to_ftp(local_file, ftp_server, ftp_path, username, password):
+    with FTP(ftp_server) as ftp:
+        ftp.login(username, password)
+        ftp.cwd(ftp_path)
+        with open(local_file, "rb") as file:
+            ftp.storbinary("STOR " + os.path.basename(local_file), file)
+
+if __name__ == "__main__":
   app.run(port=5000)
